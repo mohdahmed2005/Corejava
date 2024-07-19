a0f76d6a7522da7e3289bdb9f700a985b9b06290
mohdahmed2005 <mohammedahmed200507@gmail.com>
2024-07-19 18:12:05 +0400

    hyde

---
+++ DemoProject1/Hyde.java
@@ -0,0 +1,8 @@
+
+public class Hyde {
+
+	public static void main(String[] args) {
+System.out.println("hyderabad");
+	}
+
+}
--- DemoProject1/Welcome1234.java
+++ DemoProject1/Welcome1234.java
@@ -2,7 +2,7 @@
 public class Welcome1234 {
 
 	public static void main(String[] args) {
-System.out.println("welcome1234 file update");
+System.out.println("welcome1234 file updatec");
 	}
 
 }
