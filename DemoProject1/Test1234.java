2ffeab4ec5e3b536952f87ad8ef0941eb62a7899
mohdahmed2005 <mohammedahmed200507@gmail.com>
2024-07-19 15:00:52 +0400

    new update

--- DemoProject1/Test1234.java
+++ DemoProject1/Test1234.java
@@ -4,6 +4,7 @@ public class Test1234 {
 	public static void main(String[] args) {
 System.out.println("insidetest12345");
 System.out.println("inside lenova laprtop");
+System.out.println("inside think pad");
 	}
 
 }
